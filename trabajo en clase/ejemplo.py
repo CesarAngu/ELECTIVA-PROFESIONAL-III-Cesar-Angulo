a1ff723adcaee7d27fb7565a1c1df76c67fe0939
CesarAngu <130942750+CesarAngu@users.noreply.github.com>
2025-08-18 12:11:01 -0500

    taller 1 y 2

--- trabajo en clase/ejemplo.py
+++ trabajo en clase/ejemplo.py
@@ -3,4 +3,5 @@ print("hola mundo")
 
 #entradas
 nombre = input("cual es tu nombre?")
-print(nombre)
+print(nombre[0]) #solo imprime el primer caracter 
+print(nombre[0:2]) #solo imprime los tres primeros 
